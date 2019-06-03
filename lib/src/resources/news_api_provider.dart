32f6dda2603ae899917375d6bed1ad80cfb918c5
DARWIN V TOMY <darwinvtomy@gmail.com>
2019-06-03 17:48:42 +0530

    Initial commit

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_news/src/models/item_model.dart';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import 'dart:async';
+import 'repository.dart';
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider implements Source{
+  Client client = Client();
+
+  Future<List<int>> fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json?print=pretty');
+    final ids = json.decode(response.body);
+
+    return ids.cast<int>();//Casting is used here
+  }
+
+ Future<ItemModel> fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json?print=pretty');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJason(parsedJson);
+  }
+}
---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,67 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+import 'repository.dart';
+
+class NewsDbProvider implements Source,Cache{
+  Database db;
+
+   NewsDbProvider(){
+     init();
+   }
+
+  void init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'items.db');
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database newDb, int version) {
+      newDb.execute("""
+      CREATE TABLE Items
+        (
+          id INTEGER PRIMARY KEY,
+          type TEXT,
+          by TEXT,
+          time INTEGER,
+          text TEXT,
+          parent INTEGER,
+          kids BLOB,
+          dead INTEGER,
+          deleted INTEGER,
+          url TEXT,
+          title TEXT,
+          descendants INTEGER
+        )
+    """);
+    });
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      'Items',
+      columns: null,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+ Future<int> addItem(ItemModel item) {
+    return db.insert('Items', item.toMapForDb());
+  }
+
+  @override
+  Future<List<int>> fetchTopIds() {
+    // TODO: implement fetchTopIds
+    return null;
+  }
+}
+
+final newsDbProvider = NewsDbProvider();
